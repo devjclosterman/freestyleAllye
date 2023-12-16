a5fd0441f284e5a10218663d404a7fe5b3399543
Jason Closterman <devjclosterman@gmail.com>
2023-12-16 07:19:36 -0700

    first commit

---
+++ themes/_s/header.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
+
+	<header id="masthead" class="site-header">
+	
+	<header>
+    <nav>
+      <a href="#page1">Page 1</a>
+      <a href="#page2">Page 2</a>
+    </nav>
+    <div class="logo">
+      <img src="your_logo.png" alt="Logo">
+      <h1>Your Name</h1>
+    </div>
+    <nav>
+      <a href="#page3">Page 3</a>
+      <a href="#page4">Page 4</a>
+    </nav>
+  </header>
+  
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
